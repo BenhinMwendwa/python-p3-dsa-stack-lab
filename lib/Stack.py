4bba996655dcd7f011fc5a2db8d8c44c6d4d59d6
Benhin Mwendwa <benhinmwendwa7@gmail.com>
2024-06-11 11:52:02 +0300

    dsa

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,66 @@
 class Stack:
 
-    def __init__(self, items = [], limit = 100):
-        pass
+    def __init__(self, limit=None):
+        self.items = []
+        self.limit = limit  # Set the limit attribute
 
-    def isEmpty(self):
-        pass
+     def test_push(self):
+        '''Push 0 to stack'''
+        stk = Stack([1,2,3,4,5])
+        stk.push(0)
+        expected = [1,2,3,4,5,0]
+        for index in range(0,len(expected)):
+            assert(expected[index] == stk.items[index])
 
-    def push(self, item):
-        pass
+    def test_pop(self):
+        '''Pop 1 off the stack'''
+        stk = Stack([1,2,3,4,5])
+        stk.pop()
+        expected = [1,2,3,4]
+        for index in range(len(expected)):
+            assert(expected[index] == stk.items[index])
 
-    def pop(self):
-        pass
+    def test_size(self):
+        '''Test Stack size() method'''
+        stk = Stack([1,2,3,4,5])
+        expected = [1,2,3,4,5]
+        assert(stk.size() == len(expected))
 
-    def peek(self):
-        pass
-    
-    def size(self):
-        pass
+    def test_empty(self):
+        '''Test Stack empty() method'''
+        stk = Stack()
+        assert(stk.isEmpty())
+        assert(stk.size() == 0)
+        assert(stk.pop() == None)
+        stk.push(1)
+        assert(not stk.isEmpty())
+        assert(stk.size() == 1)
+        assert(stk.pop() == 1)
 
-    def full(self):
-        pass
 
-    def search(self, target):
-        pass
+    def test_full(self):
+        '''Test Stack full() method'''
+        stk = Stack([1], 1)
+
+        assert(stk.full())
+        assert(stk.size() == 1)
+        assert(stk.pop() == 1)
+        stk.push(1)
+        stk.push(2)
+        assert(stk.full())
+        assert(stk.size() == 1)
+        assert(stk.pop() == 1)
+
+    def test_search(self):
+        '''Test Stack search() method. How far is the element in the stack? '''
+        stk = Stack([5,6,7,8,9,10])
+
+        assert(stk.search(5) == 5)
+        assert(stk.search(6) == 4)
+        assert(stk.search(7) == 3)
+        assert(stk.search(8) == 2)
+        assert(stk.search(9) == 1)
+        assert(stk.search(10) == 0)
+
+        # Case with target not in Stack
+        assert(stk.search(15) == -1)
